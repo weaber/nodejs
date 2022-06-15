927026545bc58361e15f3454e6bb8191c5ef59a4
weaber <pnyxer@mail.ru>
2022-06-15 23:05:22 +0300

    Удалил лишнее и установил express.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const http = require('http');
-const routes = require('./routes.js');
 
-const server = http.createServer(routes.handler);
+const server = http.createServer();
 
 server.listen(3000);
