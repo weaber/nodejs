58ba55171dd4f2ebc8e91f04514992a9c13aae50
weaber <pnyxer@mail.ru>
2022-06-15 21:26:30 +0300

    Установил nodemon

--- routes.js
+++ routes.js
@@ -31,7 +31,7 @@ const requestHandler = (req, res) => {
             const message = parsedBody.split('=')[1];
             fs.writeFile('message.txt', message, err => {
                 res.statusCode = 302;
-                res.setHeader('Location', '/');
+                res.setHeader('Location', '/');                
                 return res.end();
             });
         });
