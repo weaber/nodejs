cf15244bfd9e9b044acf1d33fb4ff053b85ec338
weaber <pnyxer@mail.ru>
2022-06-16 20:27:29 +0300

    Parsing incoming requests, bodyParser installed

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 
-app.use('/users', (req, res, next) => {
-    console.log('users middleware', req.originalUrl);
-    res.send('<p>This is a /users page</p>');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title">
+      <button type="Submit">Add product</button>
+    </form>
+    `);
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    fs.appendFile('products.txt', JSON.stringify(req.body), () => {});
+    res.redirect('/');    
 });
 
 app.use('/', (req, res, next) => {
-    console.log('/ middleware', req.originalUrl);
-    res.send('<p>This is a / page</p>');    
+    res.send('<h1>Hello from Express</h1>');    
 });
 
 app.listen(3000);
