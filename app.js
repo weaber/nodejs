ac90ef1c478f603c27daf63e8949548ef6bf6b36
weaber <pnyxer@mail.ru>
2022-06-17 17:17:33 +0300

    view added

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const path = require('path');
 
 const app = express();
 
@@ -10,5 +10,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));    
+});
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title">
-      <button type="Submit">Add product</button>
-    </form>
-    `);
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/product', (req, res, next) => {
-    console.log(req.body);
+    // console.log(req.body);
     fs.appendFile('products.txt', JSON.stringify(req.body), () => {});
     res.redirect('/');    
 });
