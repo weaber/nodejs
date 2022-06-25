59739da09f810343567ae6e8a3ddf08216a8caba
weaber <pnyxer@mail.ru>
2022-06-25 09:27:34 +0300

    Storing data in file WIP

--- controllers/products.js
+++ controllers/products.js
@@ -14,10 +14,11 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-    res.render('shop', {
-        pageTitle: 'The Shop', 
-        prods: products,
-        path: '/'
-    });    
-}
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            pageTitle: 'The Shop', 
+            prods: products,
+            path: '/'
+        });
+    });
+};
--- models/product.js
+++ models/product.js
@@ -1,15 +1,41 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
     constructor(title) {
-        this._title = title;
+        this.title = title;
     }
 
     save() {
-        products.push(this);
+        const storePath = path.join(
+            path.dirname(require.main.filename), 
+            'data', 
+            'products.json'
+        );
+        fs.readFile(storePath, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(storePath, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        const storePath = path.join(
+            path.dirname(require.main.filename), 
+            'data', 
+            'products.json'
+        );
+        fs.readFile(storePath, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        })      
+        
     }
 }
